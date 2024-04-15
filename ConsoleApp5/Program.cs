3b845b0f11d654ea1333d2d950e797ab9ef8ac41
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-04-15 10:15:40 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var student = new Student("haya", "ce", 3.8);
+student.PrintInformation();
+public class Student { 
+
+    public string Name { get; set; }
+    public string Major { get; set; }
+    public double GPA { get; set; }
+
+    public Student(string name, string major, double gpa)
+    
+        {
+        Name = name;
+        Major = major;
+        GPA = gpa;
+
+        }
+    
+    public void PrintInformation()
+    {
+        Console.WriteLine($"Name : {Name}");
+        Console.WriteLine($"Major : {Major}");
+        Console.WriteLine($"GPA : {GPA}");
+
+    }
+}
